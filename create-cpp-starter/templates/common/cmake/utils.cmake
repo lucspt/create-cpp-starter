291f1bf2ac4f798cd5911b76c17274ec75452a4c
lucspt <149544385+lucspt@users.noreply.github.com>
2024-09-15 23:04:10 -0700

    feat: add utils for automating tests with cmake

--- create-cpp-starter/templates/common/cmake/utils.cmake
+++ create-cpp-starter/templates/common/cmake/utils.cmake
@@ -1,6 +1,5 @@
 include(CheckCXXCompilerFlag)
 
-####################################################################
 # Check if the given flag is supported and append it 
 # to output var if it is
 # https://github.com/pytorch/pytorch/blob/main/cmake/public/utils.cmake#L473
@@ -19,18 +18,16 @@ function(append_cxx_flag_if_supported flag outputvar)
   endif()
 endfunction()
 
-####################################################################
 # add coverage compile options if config is debug
 function(target_compile_coverage target)
-  set(COVERAGE_${target}_COMPILE_FLAGS "--coverage")
+  set(COVERAGE_${target}_COMPILE_FLAGS -O0 --coverage -g)
   append_cxx_flag_if_supported("-fprofile-abs-path" COVERAGE_${target}_COMPILE_FLAGS)
   target_compile_options(${target} PRIVATE $<$<CONFIG:Debug>:${COVERAGE_${target}_COMPILE_FLAGS}>)
   target_link_options(${target} PRIVATE $<$<CONFIG:Debug>:${COVERAGE_${target}_COMPILE_FLAGS}>)
 endfunction()
 
-####################################################################
-# Add common development compile options like -Werror to a target
-function(target_compile_debug target)
+# Add common development compiler options like -Werror to a target
+function(target_handle_compile_options target)
   target_compile_options(
     ${target}
     PRIVATE
@@ -40,4 +37,35 @@ function(target_compile_debug target)
   )
     
   get_target_property(FLAGS ${target} COMPILE_OPTIONS)
+endfunction()
+
+
+# create tests from a list of sources. SOURCES should be a list of files, 
+# each file while map to an output executable of test test_${filestem}
+# for example, tests/example.cpp will turn into the executable test_example.
+# LIBRARIES should be the list of libraries to link with the test. 
+# `gtest`, `gmock` and `gtest_main` are all  included automatically. 
+# This list this can be empty.
+#
+# create_tests_from_sources(
+#   SOURCES tests_dir/test1.test.cpp test_dir/test2.test.cpp      # the filenames of each test
+#    LIBRARIES randomlib                                          # the libraries to link to each target generated
+# )
+function(create_tests_from_sources)
+  cmake_parse_arguments(
+    Args
+    ""
+    ""
+    "SOURCES;LIBRARIES"
+    ${ARGN}
+  )
+  foreach(test_src ${Args_SOURCES})
+    cmake_path(GET test_src STEM test_filename)
+    set(test_name "test_${test_filename}")
+    add_executable(${test_name} "${test_src}")
+    add_test(NAME ${test_name} COMMAND $<TARGET_FILE:${test_name}>)
+    target_include_directories(${test_name} PRIVATE ${PROJECT_NAME})
+    target_link_libraries(${test_name} ${Args_LIBRARIES} gtest gmock gtest_main)
+    target_compile_coverage(${test_name})
+  endforeach()
 endfunction()
